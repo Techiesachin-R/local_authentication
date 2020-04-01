8f0ec58180660e55a8a3d29a128d23d56e10702e
sachin.rangdall <sachin.rangdall@gmail.com>
2020-04-01 16:17:46 +0530

    Final source code

---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Sign In'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  void initState() { 
+    _authorizeNow();
+    super.initState();
+    
+  }
+
+  final LocalAuthentication _localAuthentication = LocalAuthentication();
+
+  bool _canCheckBiometric = false;
+  String _authorizedOrNot = "Not Authorized";
+  List<BiometricType> _availableBiometricTypes = List<BiometricType>();
+
+  Future<void> _checkBiometric() async {
+    bool canCheckBiometric = false;
+    try {
+      canCheckBiometric = await _localAuthentication.canCheckBiometrics;
+    } on PlatformException catch (e) {
+      print(e);
+    }
+    if (!mounted) return;
+
+    setState(() {
+      _canCheckBiometric = canCheckBiometric;
+    });
+  }
+
+  Future<void> _getListOfBiometricTypes() async {
+    List<BiometricType> listofBiometrics;
+    try {
+      listofBiometrics = await _localAuthentication.getAvailableBiometrics();
+    } on PlatformException catch (e) {
+      print(e);
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _availableBiometricTypes = listofBiometrics;
+    });
+  }
+
+  Future<void> _authorizeNow() async {
+    bool isAuthorized = false;
+    try {
+      isAuthorized = await _localAuthentication.authenticateWithBiometrics(
+        localizedReason: "Please authenticate to complete your transaction",
+        useErrorDialogs: true,
+        stickyAuth: true,
+      );
+    } on PlatformException catch (e) {
+      print("--------------------------------------------");
+      print(e.message);
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      if (isAuthorized) {
+        _authorizedOrNot = "Authorized";
+      } else {
+        _authorizedOrNot = "Not Authorized";
+      }
+    });
+  }
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Can we check Biometric : $_canCheckBiometric"),
+            RaisedButton(
+              onPressed: _checkBiometric,
+              child: Text("Check Biometric"),
+              color: Colors.blue,
+              colorBrightness: Brightness.light,
+            ),
+            Text("List Of Biometric : ${_availableBiometricTypes.toString()}"),
+            RaisedButton(
+              onPressed: _getListOfBiometricTypes,
+              child: Text("List of Biometric Types"),
+              color: Colors.blue,
+              colorBrightness: Brightness.light,
+            ),
+            Text("Authorized : $_authorizedOrNot"),
+            RaisedButton(
+              onPressed: _authorizeNow,
+              child: Text("Authorize now"),
+              color: Colors.blue,
+              colorBrightness: Brightness.light,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
